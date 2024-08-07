99f12d5956bb4f8b79ec9193a23f808b4d9f9276
llawrence729 <163179460+llawrence729@users.noreply.github.com>
2024-08-07 09:52:19 -0400

    Create cool.js

---
+++ cool.js
@@ -0,0 +1,57 @@
+  textSize(30);
+text("Buckshot roullete", 100, 50);
+ellipse(200, 200, 200, 200);
+var hp =3 ;
+
+  var x=randomNumber(1, 5);
+var p=prompt("pick number 1-5:");
+if (x==p) { prompt("-1 hp");hp - 1;
+if (hp==0) {prompt("you dead");
+  
+}
+
+} else {prompt("You survived");
+}
+
+  var x=randomNumber(1, 5);
+var p=prompt("pick number 1-5:");
+if (x==p) { prompt("-1 hp");hp - 1;
+if (hp==0) {prompt("you dead");
+  
+}
+
+} else {prompt("You survived");
+}
+
+ var x=randomNumber(1, 5);
+var p=prompt("pick number 1-5:");
+if (x==p) { prompt("-1 hp");hp - 1;
+if (hp==0) {prompt("you dead");
+  
+}
+
+} else {prompt("You survived");
+}
+
+ var x=randomNumber(1, 5);
+var p=prompt("pick number 1-5:");
+if (x==p) { prompt("-1 hp");hp - 1;
+if (hp==0) {prompt("you dead");
+  
+}
+
+} else {prompt("You survived");
+}
+
+ var x=randomNumber(1, 5);
+var p=prompt("pick number 1-5:");
+if (x==p) { prompt("-1 hp");hp - 1;
+if (hp==0) {prompt("you dead");
+  
+}
+
+} else {prompt("You survived");
+}
+if (hp==0) {prompt("you dead");
+  
+}
